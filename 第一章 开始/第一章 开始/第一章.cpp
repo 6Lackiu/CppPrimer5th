08b6e808a8de156f746a092177a289f342f021e8
Liu <819209862@qq.com>
2021-11-17 22:10:08 +0800

    第一章 第一次提交

---
+++ 第一章 开始/第一章 开始/第一章.cpp
@@ -0,0 +1,25 @@
+//练习1.11：编写程序，提示用户输入两个整数，打印出这两个整数所指定范围内的所有整数
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num1 = 0, num2 = 0;
+	cout << "请输入有小至大两个整数：";
+	cin >> num1 >> num2;
+
+	if (num1 > num2)
+	{
+		cout << "输入有误！" << endl;
+	}
+	for (int i = num1; i <= num2; ++i)
+	{
+		cout << i << ' ';
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
+*/
