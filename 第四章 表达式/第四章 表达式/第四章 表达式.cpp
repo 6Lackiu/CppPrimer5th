692bf49714c87129620fe603081ba4912408b9e3
Liu <819209862@qq.com>
2021-11-17 22:23:14 +0800

    第四章 第一次提交

---
+++ 第四章 表达式/第四章 表达式/第四章 表达式.cpp
@@ -0,0 +1,114 @@
+//Á·Ï°4.21
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+	vector<int> v;
+	for (int i = 0; i < 10; ++i) {
+		v.push_back(i);
+	}
+
+	for (auto& c : v) {
+		(c % 2) ? c *= 2 : c;
+		cout << c << ' ';
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
+*/
+
+
+
+//Á·Ï°4.21
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+
+
+	system("pause");
+	return 0;
+}
+*/
+
+
+
+//Á·Ï°4.21
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+
+
+	system("pause");
+	return 0;
+}
+*/
+
+
+
+
+//Á·Ï°4.21
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+
+
+	system("pause");
+	return 0;
+}
+*/
+
+
+
+
+//Á·Ï°4.21
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+
+
+	system("pause");
+	return 0;
+}
+*/
+
+
+
+//Á·Ï°4.21
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+
+
+	system("pause");
+	return 0;
+}
+*/
+
+
+
+
+//Á·Ï°4.21
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+
+
+	system("pause");
+	return 0;
+}
+*/
